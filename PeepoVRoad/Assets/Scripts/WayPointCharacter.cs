9be3758b859bcc9c680a2baec68054577f2b9432
UO264802 <71766858+UO264802@users.noreply.github.com>
2021-11-07 17:00:09 +0100

    Generación coches

---
+++ PeepoVRoad/Assets/Scripts/WayPointCharacter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointCharacter : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform target;
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+     float gravity = -9.81f;
+    private Vector3 velocity;
+  
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 relativePos = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up * Time.deltaTime);
+        transform.rotation = rotation;
+
+        controller.Move(transform.forward * speed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    public void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        
+        if (hit.gameObject.CompareTag("WayPoint"))
+        {
+            Debug.Log("Colision con waypoint");
+            target = hit.gameObject.GetComponent<WayPoint>().nextWayPoint;
+        }
+    }
+}
