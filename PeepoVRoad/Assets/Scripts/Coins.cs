5fbdd3a1081b814788e91ece5db5ee6f99262f86
UO269984 <UO269984@uniovi.es>
2022-01-07 02:02:46 +0100

    Rotación de monedas

--- PeepoVRoad/Assets/Scripts/Coins.cs
+++ PeepoVRoad/Assets/Scripts/Coins.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Coins : MonoBehaviour
 {
     public AudioSource audioSource;
+    public float rotSpeed = 0.5f;
 
     void Start () {
         audioSource = GetComponent<AudioSource>();
     }
-
+    
+    public void Update() {
+        transform.Rotate(0, 360 * this.rotSpeed * Time.deltaTime, 0);
+    }
+    
     void OnTriggerEnter(Collider collision){
 
         if (collision.gameObject.tag == "Player"){
