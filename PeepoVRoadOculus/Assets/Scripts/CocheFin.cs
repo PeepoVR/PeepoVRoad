8fc125d468949332835f4b807d14aba3f3854e61
uo266640 <jony23699@gmail.com>
2022-01-10 20:14:58 +0100

    actualización nivel 1

---
+++ PeepoVRoadOculus/Assets/Scripts/CocheFin.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CocheFin : MonoBehaviour
+{
+     private void OnTriggerEnter(Collider colideObj)
+    {
+        if (ContadorMonedas.contadorMonedas >= 5)
+            SceneManager.LoadSceneAsync("Level2");
+    }
+}
