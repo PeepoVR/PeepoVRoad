179306690dd48cb67c670c16b88eccf545337bd3
UO269984 <UO269984@uniovi.es>
2022-01-11 13:53:38 +0100

    Arreglado contador de monedas
    Ahora el contador se reinicia correctamente al reiniciar el nivel

--- PeepoVRoad/Assets/Scripts/ContadorMonedas.cs
+++ PeepoVRoad/Assets/Scripts/ContadorMonedas.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ContadorMonedas : MonoBehaviour
-{
-    static public int contadorMonedas;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class ContadorMonedas : MonoBehaviour {
+	static public int contadorMonedas = 0;
+	
+	void Start() {
+		contadorMonedas = 0;
+	}
 }
